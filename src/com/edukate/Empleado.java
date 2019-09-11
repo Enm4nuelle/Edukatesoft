4831c54999333e9a50bdc112c6aaf7f0f3764abd
alulab14 <alulab14@inf.pucp.edu.pe>
2019-09-11 17:06:02 -0500

    Empleado modificado a la espera de Usuario

--- src/com/edukate/Empleado.java
+++ src/com/edukate/Empleado.java
@@ -1,3 +1,4 @@
+package com.edukate;
 public abstract class Empleado{
 	//Variable constante que nos permitira generar un id correlativo
 	private static int codigoGenerado = 1; 
@@ -12,37 +13,76 @@ public abstract class Empleado{
 	private char estadoCivil;
 	private float sueldo;
 	private Usuario user;
-	public Empleado(){
-		user = new Usuario();
-		this.id = codigoGenerado;
-		codigoGenerado++;
-	}
+	public Empleado(){}
 	public Empleado(String nombre, String apellidos, String dni, int edad, String telefono, String direccion, String correo, 
 					char estadoCivil, float sueldo){}
-	public int getId(){}
-	public void setId(int id){}
-	public String getNombre(){}
-	public void setNombre(String nombre){}
-	public String setApellidos(){}
-	public void getApellidos(String apellidos){}
-	public String getDni(){}
-	public void setDni(String dni){}
-	public int getEdad(){}
-	public void setEdad(int edad){}
-	public String getTelefono(){}
-	public void setTelefono(String telefono){}
-	public String getDireccion(){}
-	public void setDireccion(String direccion){}
-	public String getCorreo(){}
-	public void setCorreo(String correo){}
-	public char getEstadoCivil(){}
-	public void setEstadoCivil(char estadoCivil){}
-	public float getSueldo(){}
-	public void setSueldo(float sueldo){}
-	public Usuario getUser(){}
-	public void setUser(Usuario user){}
-	
-	abstract float calcularSueldo(){
+	public int getId(){
+		return id;
 	}
+	public void setId(int id){
+		this.id = id;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public String setApellidos(){
+		return apellidos;
+	}
+	public void getApellidos(String apellidos){
+		this.apellidos = apellidos;
+	}
+	public String getDni(){
+		return dni;
+	}
+	public void setDni(String dni){
+		this.dni = dni;
+	}
+	public int getEdad(){
+		return edad;
+	}
+	public void setEdad(int edad){
+		this.edad = edad;
+	}
+	public String getTelefono(){
+		return telefono;
+	}
+	public void setTelefono(String telefono){
+		this.telefono = telefono;
+	}
+	public String getDireccion(){
+		return direccion;
+	}
+	public void setDireccion(String direccion){
+		this.direccion = direccion;
+	}
+	public String getCorreo(){
+		return correo;
+	}
+	public void setCorreo(String correo){
+		this.correo = correo;
+	}
+	public char getEstadoCivil(){
+		return estadoCivil;
+	}
+	public void setEstadoCivil(char estadoCivil){
+		this.estadoCivil = estadoCivil;
+	}
+	public float getSueldo(){
+		return sueldo;
+	}
+	public void setSueldo(float sueldo){
+		this.sueldo = sueldo;
+	}
+	public Usuario getUser(){
+		return user;
+	}
+	public void setUser(Usuario user){
+		this.user = user;
+	}
+
+	public abstract float calcularSueldo();
 	
 }
