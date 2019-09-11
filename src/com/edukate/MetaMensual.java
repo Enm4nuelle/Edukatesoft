8c73d67b09a331ddfdc23df5a93cb14b708165cf
alulab14 <alulab14@inf.pucp.edu.pe>
2019-09-11 17:24:01 -0500

    Ingreso de la clase MetaMensual

--- src/com/edukate/MetaMensual.java
+++ src/com/edukate/MetaMensual.java
@@ -2,5 +2,30 @@ package com.edukate;
 
 public class MetaMensual {
     private date fechaInicio;
-    private date fechaFinal;
+    private date fechaLimite;
+    private float cantObjetivo;
+
+    public date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public date getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setFechaLimite(date fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+
+    public float getCantObjetivo() {
+        return cantObjetivo;
+    }
+
+    public void setCantObjetivo(float cantObjetivo) {
+        this.cantObjetivo = cantObjetivo;
+    }
 }
